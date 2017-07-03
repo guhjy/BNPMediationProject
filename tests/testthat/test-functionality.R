2a5dda50f222f553571013642a80ecca97027de8
mjdaniels <mjdaniels@austin.utexas.edu>
2017-07-02 19:50:25 -0500

    adding test

---
+++ tests/testthat/test-functionality.R
@@ -0,0 +1,12 @@
+context("functionality")
+
+test_that("test are here", {
+  expect_equal(2 * 2, 4)
+})
+
+
+#test_that("test that output is of correct type",
+#         { expect_type(bnpm(dataTreatment = w1, dataControl = w0, prior = prior, mcmc = mcmc, state = state), "list")
+#           expect_type(bnpconmediation(dataTreatment, dataControl,prior, mcmc, state, status=TRUE,na.action, q=2, NN=10, n1=10, n0=10, extra.thin=0, cond.values=c(45,35), col.values=c(1,2))), "list")
+#
+# })
